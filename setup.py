2c03cd9aad9109328a8b2ce46f0811472cc2e4c8
Ronald Rounsifer <ronrounsifer@me.com>
2017-12-31 15:09:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,15 @@
 from distutils.core import setup
 
 setup(
-    name = 'CryptoCompareWrapper',
+    name = 'CryptoComparePython',
     version = '1.0.0',
-    url = 'https://github.com/iJEEBUS/CryptoCompareWrapper',
-    download_url = 'https://github.com/iJEEBUS/CryptoCompareWrapper/archive/master.zip',
+    url = 'https://github.com/iJEEBUS/CryptoComparePython',
+    download_url = 'https://github.com/iJEEBUS/CryptoComparePython/archive/master.zip',
     author = 'Ronald Rounsifer : ronrounsifer@me.com',
     author_email = 'ronrounsifer@me.com',
     license = 'MIT',
-    packages = ['CryptoCompareWrapper'],
+    packages = ['CryptoComparePython'],
     description = 'Python wrapper for cryptocompare.com API.',
-    long_description = open('README.md','r').read(),
     install_requires=['requests', 'json'],
     keywords = ['cryptocurrency', 'API', 'cryptocompare','BTC', 'Bitcoin', 'LTC', 'Litecoin', 'DOGE', 'Dogecoin', 'ETH', 'Ethereum'],
 )
