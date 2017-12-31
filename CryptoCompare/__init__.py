aa517ef52eaeea11c5a6e9718a8816f6c4ea2850
Ronald Rounsifer <ronrounsifer@me.com>
2017-12-31 15:02:47 -0500

    Update __init__.py

--- CryptoCompare/__init__.py
+++ CryptoCompare/__init__.py
@@ -4,7 +4,7 @@
 __title__ = 'CryptoCompareWrapper'
 __version__ = '1.0.0'
 __author__ = 'Ronald Rounsifer : ronrounsifer@me.com'
-__repo__ = 'https://github.com/iJEEBUS/CryptoCompareWrapper'
+__repo__ = 'https://github.com/iJEEBUS/CryptoComparePython'
 __license__ = 'MIT'
 
 from .wrapper import Wrapper
